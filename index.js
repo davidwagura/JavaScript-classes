96efb8d648a15c5959f06a6439c29b4fdd6a2528
David wanjohi <davidwagura83@gmail.com>
2024-01-24 12:19:02 +0300

    program to calculate the area and perimeter of a rectangle

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ class Person {
         this.age = age;
         this.country = country;
     }
+
     displayDetails(){
-        return (this.name + " " + this.age + " " + this.country )
+        return this.name + " " + this.age + " " + this.country;
     }
 }
 
-const person1 = new Person ('David', 18, 'Kenya');
-const person2 = new Person ('Mary', 25, 'USA');
+const person1 = new Person('David', 18, 'Kenya');
+const person2 = new Person('Mary', 25, 'USA');
 
 
-console.log(person1);
+console.log(person1.displayDetails());
+
+class Rectangle {
+    constructor (width, height, length){
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    calculateArea(){
+        return this.length * this.width * this.height;
+    }
+
+    calculatePerimeter(){
+        return 2 * (this.length + this.width);
+    }
+}
+
+const rectangle1 = new Rectangle(2, 4, 6);
+
+
+console.log(rectangle1.calculateArea());
+console.log(rectangle1.calculatePerimeter());
